be15226a9c603d916412a4ae6e4fc4c064aad91c
NinoGveno <nino.gvenetadze1@gmail.com>
2023-04-13 14:25:25 +0400

    push

---
+++ app.js
@@ -0,0 +1,13 @@
+let myNumber = 10;
+let myString = "Hello World!";
+let myBoolean = true;
+let myUndefined = undefined;
+let myNull = null;
+let myComputerManufacturer = "Apple";
+let myComputerMemory = 32;
+let myComputerOS = "macOS";
+let computerBuyingYear = 2022;
+let currentYear = new Date().getFullYear();
+let yearsOfUsing = currentYear - computerBuyingYear;
+let sentence = `My computer is ${myComputerManufacturer}, it runs on ${myComputerOS} and I've been using it for ${yearsOfUsing} years.`
+console.log(sentence);
